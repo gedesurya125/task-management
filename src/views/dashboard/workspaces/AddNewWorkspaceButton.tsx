6dd33206bcb16fc17eee123e33f27eb86a1245ff
gedesurya125 <gedesurya125@gmail.com>
2024-03-23 16:55:26 +0800

    add formik to the create workspace modal

--- src/views/dashboard/workspaces/AddNewWorkspaceButton.tsx
+++ src/views/dashboard/workspaces/AddNewWorkspaceButton.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Card, CardTitle } from "@/components/ui/card";
+import { string, object } from "yup";
 import React from "react";
 import { Plus } from "lucide-react";
 import {
@@ -13,6 +15,12 @@ import {
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Formik, Form, useField } from "formik";
+
+const workspaceSchema = object({
+  title: string().required(),
+  description: string().required(),
+});
 
 export const AddNewWorkspaceButton = () => {
   return (
@@ -32,32 +40,61 @@ export const AddNewWorkspaceButton = () => {
           <DialogTitle>New Workspace</DialogTitle>
           <DialogDescription>Fill the workspace Detail here</DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="title" className="text-right">
-              Title
-            </Label>
-            <Input
-              id="title"
-              defaultValue="Awesome Workspace"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="description" className="text-right">
-              Description
-            </Label>
-            <Input
-              id="description"
-              defaultValue="My Awesome workspace"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Create</Button>
-        </DialogFooter>
+        <Formik
+          enableReinitialize
+          initialValues={{
+            title: "",
+            description: "",
+          }}
+          onSubmit={(values) => {
+            console.log("this is the formik values", values);
+          }}
+          validationSchema={workspaceSchema}
+        >
+          {({ handleBlur, handleChange, values }) => {
+            return (
+              <Form>
+                <div className="grid gap-4 py-4">
+                  <TextInput id="title" name="title" label="title" />
+                  <TextInput
+                    id="description"
+                    name="description"
+                    label="description"
+                  />
+                </div>
+                <DialogFooter>
+                  <Button type="submit">Create</Button>
+                </DialogFooter>
+              </Form>
+            );
+          }}
+        </Formik>
       </DialogContent>
     </Dialog>
+  );
+};
+
+const TextInput = ({
+  id,
+  name,
+  label,
+}: {
+  id: string;
+  name: string;
+  label: string;
+}) => {
+  const [field, meta, helper] = useField(name);
+  return (
+    <div className="grid grid-cols-4 items-center gap-x-4">
+      <Label htmlFor={id} className="text-right">
+        {label}
+      </Label>
+      <Input id={id} className="col-span-3" {...field} />
+      {meta.touched && meta.error ? (
+        <span className="error col-start-2 col-span-3 text-destructive mt-1">
+          {meta.error}
+        </span>
+      ) : null}
+    </div>
   );
 };
