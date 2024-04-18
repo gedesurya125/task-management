7f812ada359f842d0884a07a95518f7cf3aa2ae4
gedesurya125 <gedesurya125@gmail.com>
2024-04-18 22:59:51 +0800

    add uncompleted Confirmation Dialog

---
+++ src/components/ConfirmationDialog.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+export const ConfirmationDialog = ({
+  children,
+}: {
+  children: React.ReactNode;
+  title: string;
+  message: string;
+  cancelButtonText?: string;
+  continueButtonText?: string;
+  cancelAction?: () => void;
+  continueAction?: () => void;
+}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
