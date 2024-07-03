dc3953c5001036134356d34c0f867388141bb929
gedesurya125 <gedesurya125@gmail.com>
2024-07-03 08:16:16 +0800

    add more dummy data for the list card

--- src/data/fakeListItem.ts
+++ src/data/fakeListItem.ts
@@ -1,30 +1,51 @@
 export const fakeListItem = [
   {
+    id: "list-1",
     title: "list one ",
-    description:
-      "list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n list one description \n",
+    description: "list one description",
+    tasks: [
+      { title: "task one", id: "list-task-1" },
+      { title: "task two", id: "list-task-2" },
+      { title: "task three", id: "list-task-3" },
+      { title: "task four", id: "list-task-4" },
+      { title: "task five", id: "list-task-5" },
+      { title: "task six", id: "list-task-6" },
+    ],
   },
   {
+    id: "list-2",
+
     title: "list two ",
     description: "list one description",
+    tasks: [],
   },
   {
+    id: "list-3",
+
     title: "list three ",
     description: "list one description",
   },
   {
+    id: "list-4",
+
     title: "list four",
     description: "list one description",
   },
   {
+    id: "list-5",
+
     title: "list five",
     description: "list one description",
   },
   {
+    id: "list-6",
+
     title: "list five",
     description: "list one description",
   },
   {
+    id: "list-7",
+
     title: "list five",
     description: "list one description",
   },
